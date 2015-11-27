354f6908add5ac2133f2f0bcdbc5eb2c0442a31e
ehawk823 <evan.alexander.hawk@gmail.com>
2015-11-27 01:29:42 -0600

    integrated mongo and twitter api

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'envyable'
+gem 'pry'
 gem 'mongoid'
 gem 'twitter'
 gem 'dotenv-rails'
--- app/controllers/handles_controller.rb
+++ app/controllers/handles_controller.rb
@@ -10,12 +10,13 @@ class HandlesController < ApplicationController
   # GET /handles/1
   # GET /handles/1.json
   def show
-    @client = Twitter::Streaming::Client.new do |config|
-      config.consumer_key        = "YOUR_CONSUMER_KEY"
-      config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-      config.access_token        = "YOUR_ACCESS_TOKEN"
-      config.access_token_secret = "YOUR_ACCESS_SECRET"
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
+      config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
     end
+    binding.pry
   end
 
   # GET /handles/new
