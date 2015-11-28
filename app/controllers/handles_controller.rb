74f6245c0cd6ab5b00c7b36eda0df4533729e49c
ehawk823 <evan.alexander.hawk@gmail.com>
2015-11-28 17:54:20 -0600

    refactored handles controller

--- app/controllers/handles_controller.rb
+++ app/controllers/handles_controller.rb
@@ -10,24 +10,11 @@ class HandlesController < ApplicationController
   # GET /handles/1
   # GET /handles/1.json
   def show
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
-      config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
-      config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
-    end
-    @tweets = @client.user_timeline(@handle.handle_name)
+    @tweets = @handle.tweets
     @tweets_num = @tweets.length
-    @characters = 0
-    @retweets = 0
-    @uris = 0
-    @tweets[1..20].each do |tweet|
-      @characters += tweet.text.length
-      @retweets += tweet.retweet_count
-      if tweet.uris?
-        @uris += 1
-      end
-    end
+    @retweets = @handle.retweets
+    @characters = @handle.characters
+    @uris = @handle.uris
     @character_avg = @characters / @tweets_num
   end
 
--- app/models/handle.rb
+++ app/models/handle.rb
@@ -1,4 +1,48 @@
 class Handle
   include Mongoid::Document
   field :handle_name, type: String
+
+  def client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["YOUR_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["YOUR_CONSUMER_SECRET"]
+      config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
+    end
+  end
+
+  def tweets
+    self.client
+    @client.user_timeline(self.handle_name)
+  end
+
+  def characters
+    @characters = 0
+    @tweets = self.tweets
+    @tweets[1..20].each do |tweet|
+      @characters += tweet.text.length
+    end
+    @characters
+  end
+
+  def retweets
+    @retweets = 0
+    @tweets = self.tweets
+    @tweets[1..20].each do |tweet|
+      @retweets += tweet.retweet_count
+    end
+    @retweets
+  end
+
+  def uris
+    @uris = 0
+    @tweets = self.tweets
+    @tweets[1..20].each do |tweet|
+      if tweet.uris?
+        @uris += 1
+      end
+    end
+    @uris
+  end
+
 end
